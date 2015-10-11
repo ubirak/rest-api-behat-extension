47606701088b882fd262e02dc684f4062340bfc6
David Samblas <david.samblas@sntc.no>
2015-10-11 14:06:52 +0200

    Make behat test pass

--- README.md
+++ README.md
@@ -15,7 +15,9 @@ default:
     extensions:
         Rezzza\RestApiBehatExtension\Extension:
             rest:
-                base_url: http://localhost:8888
+                client:
+                    config:
+                        base_uri : http://localhost:8888
                 store_response: true
     suites:
         default:
--- src/Rest/RestApiBrowser.php
+++ src/Rest/RestApiBrowser.php
@@ -2,18 +2,21 @@
 
 namespace Rezzza\RestApiBehatExtension\Rest;
 
-use Guzzle\Http\Exception\BadResponseException;
-use Guzzle\Http\ClientInterface as HttpClient;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\ClientInterface as HttpClient;
+use GuzzleHttp\Psr7\Request;
+use Behat\Gherkin\Node\PyStringNode;
+
 
 class RestApiBrowser
 {
     /** @var HttpClient */
     private $httpClient;
 
-    /** @var array|\Guzzle\Http\Message\RequestInterface */
+    /** @var array|\Psr\Http\Message\RequestInterface */
     private $request;
 
-    /** @var \Guzzle\Http\Message\Response|array */
+    /** @var \GuzzleHttp\Psr7\Response|array */
     private $response;
 
     /** @var array */
@@ -51,11 +54,10 @@ class RestApiBrowser
      * @param string $method
      * @param string $url
      * @param PyStringNode $body
-     * @param array $options
      */
-    public function sendRequest($method, $url, $body = null, array $options = array())
+    public function sendRequest($method, $url, $body = null)
     {
-        $this->createRequest($method, $url, $body, $options);
+        $this->createRequest($method, $url, $body);
 
         try {
             $this->response = $this->httpClient->send($this->request);
@@ -68,7 +70,7 @@ class RestApiBrowser
         }
 
         if (null !== $this->responseStorage) {
-            $this->responseStorage->writeRawContent($this->response->getBody(true));
+            $this->responseStorage->writeRawContent($this->response->getBody()->getContents());
         }
     }
 
@@ -79,10 +81,7 @@ class RestApiBrowser
     public function addRequestHeader($name, $value)
     {
         if (isset($this->requestHeaders[$name])) {
-            if (!is_array($this->requestHeaders[$name])) {
-                $this->requestHeaders[$name] = array($this->requestHeaders[$name]);
-            }
-            $this->requestHeaders[$name][] = $value;
+            $this->requestHeaders[$name] = implode(", ",array($this->requestHeaders[$name], $value));
         } else {
             $this->requestHeaders[$name] = $value;
         }
@@ -102,15 +101,18 @@ class RestApiBrowser
      * @param string                $method
      * @param string                $uri With or without host
      * @param string|resource|array $body
-     * @param array                 $options
      */
-    private function createRequest($method, $uri, $body = null, array $options = array())
+    private function createRequest($method, $uri, $body = null)
     {
         if (!$this->hasHost($uri)) {
-            $uri = rtrim($this->httpClient->getBaseUrl(), '/') . '/' . ltrim($uri, '/');
+            $uri = rtrim($this->httpClient->getConfig("base_uri"), '/') . '/' . ltrim($uri, '/');
+        }
+        if(is_null($body))
+            $this->request = new Request($method, $uri, $this->requestHeaders);
+        else {
+            $this->request = new Request($method, $uri, $this->requestHeaders, $body);
         }
 
-        $this->request = $this->httpClient->createRequest($method, $uri, $this->requestHeaders, $body, $options);
         // Reset headers used for the HTTP request
         $this->requestHeaders = array();
     }
